5b859899a2ed1d8b9018a81c7c7a98c8aa04a20f
Zokes <codzoky@gmail.com>
2020-06-12 17:24:33 +0100

    initial and final commit

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
